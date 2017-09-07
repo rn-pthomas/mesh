6c2aa9776dd9bd9afdf5d5c9c3e71ab642961574
pat-thomas <patthomassoftware@gmail.com>
2017-09-07 17:29:38 -0400

    Basic grid utilities.

--- src/mesh/core.clj
+++ src/mesh/core.clj
@@ -1,6 +1,30 @@
 (ns mesh.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-grid
+  [{:keys [height width]}]
+  (vec
+   (for [x (range height)]
+     (mapv (fn [y]
+             {:x x
+              :y y})
+           (range width)))))
+
+(defn get-cell
+  ([grid x y]
+   (-> grid (nth x) (nth y)))
+  ([grid {:keys [x y]}]
+   (get-cell grid x y)))
+
+(defn update-cell
+  ([grid x y update-fn]
+   (update-in grid [x y] update-fn))
+  ([grid {:keys [x y]} update-fn]
+   (update-cell grid x y update-fn)))
+
+(defn get-row
+  [grid y]
+  (mapv #(get % 0) grid))
+
+(defn get-column
+  [grid x]
+  (get grid x))
